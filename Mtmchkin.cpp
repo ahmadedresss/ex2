545a11cbe1cd2cb19268166dada4725ef11d6c48
mohamadkhalaily <103219002+mohamadkhalaily@users.noreply.github.com>
2022-05-13 22:34:27 +0300

    Update Mtmchkin.cpp

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -14,42 +14,13 @@ Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCard
     this->m_status=GameStatus::MidGame;
     this->m_index=0;
 }
-
+/*
 Mtmchkin::~Mtmchkin()
 {
     delete[] this->cardsArray;
-}
+}*/
 
 
-Mtmchkin::Mtmchkin(const Mtmchkin& tmp):m_player(tmp.playerName)
-{
-    m_numOfCards=tmp.m_numOfCards;
-    m_status=tmp.m_status;
-    m_index=tmp.m_index;
-    Card* arr=new Card[m_numOfCards];
-    for (int i = 0; i < m_numOfCards; ++i)
-    {
-        arr[i]=tmp.cardsArray[i];
-    }
-}
-Mtmchkin& Mtmchkin:: operator=(Mtmchkin &m)
-{
-    if (this == &m)
-    {
-        return *this;
-    }
-
-    delete[] this->cardsArray;
-    this->m_status = m.m_status;
-    this->m_index = m.m_index;
-    this->m_numOfCards = m.m_numOfCards;
-    this->m_player = m.m_player;
-    Card* arr=new Card[m_numOfCards];
-    for (int i = 0; i < m_numOfCards; ++i)
-    {
-        arr[i]=m.cardsArray[i];
-    }    return *this;
-}
 
 void Mtmchkin::playNextCard()
 {
