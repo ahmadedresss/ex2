0f51f68e08a3a4e3891fc0e84147b52b2e97a721
mohamadkhalaily <103219002+mohamadkhalaily@users.noreply.github.com>
2022-05-13 21:50:49 +0300

    Update Mtmchkin.cpp

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -12,32 +12,69 @@ Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCard
     this->cardsArray=cardsArray;
     this->m_numOfCards=numOfCards;
     this->m_status=GameStatus::MidGame;
-    this->m_index=1;
+    this->m_index=0;
+}
+
+Mtmchkin::~Mtmchkin()
+{
+    delete[] this->cardsArray;
+}
+
+
+Mtmchkin::Mtmchkin(const Mtmchkin& tmp):m_player(tmp.playerName)
+{
+    m_numOfCards=tmp.m_numOfCards;
+    m_status=tmp.m_status;
+    m_index=tmp.m_index;
+    Card* arr=new Card[m_numOfCards];
+    for (int i = 0; i < m_numOfCards; ++i)
+    {
+        arr[i]=tmp.cardsArray[i];
+    }
+}
+Mtmchkin& Mtmchkin:: operator=(Mtmchkin &m)
+{
+    if (this == &m)
+    {
+        return *this;
+    }
+
+    delete[] this->cardsArray;
+    this->m_status = m.m_status;
+    this->m_index = m.m_index;
+    this->m_numOfCards = m.m_numOfCards;
+    this->m_player = m.m_player;
+    Card* arr=new Card[m_numOfCards];
+    for (int i = 0; i < m_numOfCards; ++i)
+    {
+        arr[i]=m.cardsArray[i];
+    }    return *this;
 }
 
 void Mtmchkin::playNextCard()
 {
-    if(this->m_index > this->m_numOfCards)
-    {
-        m_index=1;
-    }
-
     Card current_card = this->cardsArray[m_index];
     current_card.printInfo();
     current_card.applyEncounter(m_player);
+    m_player.printInfo();
+
     if(m_player.isKnockedOut())
     {
         this->m_status=GameStatus::Loss;
-        return;
     }
-    if(m_player.getLevel()>=10)
+    if(m_player.getLevel()==10)
     {
         this->m_status=GameStatus::Win;
-        return;
     }
 
-    m_index++;
-    m_player.printInfo();
+    if(this->m_index > this->m_numOfCards-1)
+    {
+        m_index=0;
+    }
+    else
+    {
+        m_index++;
+    }
 }
 bool Mtmchkin::isOver() const
 {
